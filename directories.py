9b71ca7170ef2d06694c7d1d0f149ac6b39c4dec
ErikGrigoryan1 <erikgrigoryan741@gmail.com>
2025-07-09 20:01:30 +0400

    update

--- directories.py
+++ directories.py
@@ -1,23 +1,19 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 import requests
 
-
-target_url = input('[*] Enter Target URL: ')
+target_url = input('[*] Enter Target URL (include http:// or https://): ').rstrip('/')
 file_name = input('[*] Enter Name Of The File Containing Directories: ')
 
-
 def request(url):
-	try:
-		return requests.get("http://" + url)
-	except requests.exceptions.ConnectionError:
-		pass
+    try:
+        return requests.get(url)
+    except requests.exceptions.RequestException:
+        return None
 
-
-file = open(file_name, 'r')
-for line in file:
-	directory = line.strip()
-	full_url = target_url + '/' + directory
-	response = request(full_url)
-	if response:
-		print('[*] Discovered Directory At This Path: ' + full_url)
+with open(file_name, 'r') as file:
+    for line in file:
+        directory = line.strip()
+        full_url = f"{target_url}/{directory}"
+        response = request(full_url)
+        if response and response.status_code == 200:
+            print(f'[*] Discovered Directory At This Path: {full_url}')
--- functions.py
+++ functions.py
@@ -1,47 +1,49 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import os
 import platform
 import time
 import threading
 import socket
+import sys
 
 from datetime import datetime
 from queue import Queue
+
 def ping_check():
-   net = input("Enter the Network Address: ")
-   net1= net.split('.')
-   a = '.'
+    net = input("Enter the Network Address (e.g., 192.168.1.0): ")
+    net1 = net.split('.')
+    if len(net1) != 4:
+        print("Invalid network address.")
+        return
 
-   net2 = net1[0] + a + net1[1] + a + net1[2] + a
-   st1 = int(input("Enter the Starting Number: "))
-   en1 = int(input("Enter the Last Number: "))
-   en1 = en1 + 1
-   oper = platform.system()
+    net2 = f"{net1[0]}.{net1[1]}.{net1[2]}."
+    try:
+        st1 = int(input("Enter the Starting Number (e.g., 1): "))
+        en1 = int(input("Enter the Last Number (e.g., 254): "))
+    except ValueError:
+        print("Invalid input range.")
+        return
 
-   if (oper == "Windows"):
-      ping1 = "ping -n 1 "
-   else :
-      ping1 = "ping -c 1 "
-   t1 = datetime.now()
-   print ("Scanning in Progress:")
+    en1 += 1
+    oper = platform.system()
+    ping1 = "ping -n 1 " if oper == "Windows" else "ping -c 1 "
 
-   for ip in range(st1,en1):
-       addr = net2 + str(ip)
-       comm = ping1 + addr
-       response = os.popen(comm)
-      
-       for line in response.readlines():
-           if "ttl" in line.lower():
-               print(addr, "--> Live")
-               break
+    t1 = datetime.now()
+    print("Scanning in Progress...")
 
+    for ip in range(st1, en1):
+        addr = net2 + str(ip)
+        comm = ping1 + addr
+        response = os.popen(comm)
 
-   t2 = datetime.now()
-   total = t2 - t1
-   print ("Scanning completed in: ",total)
+        for line in response.readlines():
+            if "ttl" in line.lower():
+                print(addr, "--> Live")
+                break
 
-
-
+    t2 = datetime.now()
+    total = t2 - t1
+    print("Scanning completed in:", total)
 
 
 def start_scan():
@@ -49,24 +51,42 @@ def start_scan():
     print_lock = threading.Lock()
 
     target = input('Enter the host to be scanned: ')
-    t_IP = socket.gethostbyname(target)
+    try:
+        t_IP = socket.gethostbyname(target)
+    except socket.gaierror:
+        print("Invalid host.")
+        return
+
     print('Starting scan on host:', t_IP)
 
     def portscan(port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(0.5)
+        s.settimeout(1)
         try:
             s.connect((t_IP, port))
-            
-            # Try to grab banner
+
+            banner = ""
+            # Send minimal probes for some common ports to get version info
             try:
-                banner = s.recv(1024).decode().strip()
-                if not banner:
-                    banner = "No banner"
+                if port in [80, 8080]:
+                    s.sendall(b"HEAD / HTTP/1.0\r\n\r\n")
+                    banner = s.recv(1024).decode(errors="ignore").strip()
+                elif port == 21:  # FTP usually sends banner first
+                    banner = s.recv(1024).decode(errors="ignore").strip()
+                elif port == 22:  # SSH usually sends banner first
+                    banner = s.recv(1024).decode(errors="ignore").strip()
+                elif port == 25:  # SMTP usually sends banner first
+                    banner = s.recv(1024).decode(errors="ignore").strip()
+                else:
+                    # For other ports, just try to recv data if sent
+                    s.settimeout(0.5)
+                    banner = s.recv(1024).decode(errors="ignore").strip()
             except:
                 banner = "No banner"
 
-            # Try to identify service by port
+            if not banner:
+                banner = "No banner"
+
             try:
                 service = socket.getservbyport(port)
             except:
@@ -93,19 +113,19 @@ def start_scan():
         t.daemon = True
         t.start()
 
-    for worker in range(1, 500):
+    for worker in range(1, 1025):  # scanning ports 1 to 1024
         q.put(worker)
 
     q.join()
-    print('Time taken:', time.time() - startTime)
+    print('Time taken:', round(time.time() - startTime, 2), "seconds")
+
 
 if __name__ == "__main__":
-    import sys
     if len(sys.argv) > 1 and sys.argv[1] == "ping":
         ping_check()
     elif len(sys.argv) > 1 and sys.argv[1] == "scan":
         start_scan()
     else:
         print("Usage:")
-        print("  python3 functions.py ping   # to do ping scan")
-        print("  python3 functions.py scan   # to do port scan")
+        print("  python3 netscaner.py ping   # to do ping sweep")
+        print("  python3 netscaner.py scan   # to do port scan")
